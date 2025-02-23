9ea36c182f6122fe89f96593da43f1af7faa07e6
Ansh Bajaj <bajajanshbajaj1@gmail.com>
2025-02-23 12:17:32 +0530

    Update main.py

--- 07/main.py
+++ 07/main.py
@@ -12,8 +12,6 @@ asmpath = ''
 vm_dir = filedialog.askdirectory()
 print(vm_dir)
 vm_files = os.listdir(vm_dir)
-if 'Sys.vm' in vm_files:
-    instruction+= ini.callsysinit()
 
 for i in range (len(vm_files)):
     base_name = vm_files[i]
